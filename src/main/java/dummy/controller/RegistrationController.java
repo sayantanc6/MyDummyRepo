b25a17dfeb6421f171d16713b1446e5a07fdae7e
Sayantan Chatterjee <35490107+sayantanc6@users.noreply.github.com>
2020-02-16 20:17:53 +0530

    Update RegistrationController.java

--- src/main/java/dummy/controller/RegistrationController.java
+++ src/main/java/dummy/controller/RegistrationController.java
@@ -55,31 +55,6 @@ public class RegistrationController {
 	public String NewUser() {
 		return "Registration";
 	}
-	
-	@RequestMapping(value = "/registrationConfirm", method = RequestMethod.GET)
-	public String confirmRegistration(WebRequest request, Model model, @RequestParam("token") String token) {
-	  
-	    Locale locale = request.getLocale();
-	     
-	    VerificationToken verificationToken = empService.getVerificationToken(token);
-	    if (verificationToken == null) {
-	        String message = messages.getMessage("auth.message.invalidToken", null, locale);
-	        model.addAttribute("message", message);
-	        return "redirect:/badUser.html?lang=" + locale.getLanguage();
-	    }
-	     
-	    user = verificationToken.getUser();
-	    Calendar cal = Calendar.getInstance();
-	    if ((verificationToken.getExpiryDate().getTime() - cal.getTime().getTime()) <= 0) {
-	        String messageValue = messages.getMessage("auth.message.expired", null, locale);
-	        model.addAttribute("message", messageValue);
-	        return "redirect:/WEB-INF/views/badUser.jsp?lang=" + locale.getLanguage();
-	    } 
-	     
-	    ((EmployeeModel) model).setEnabled(true); 
-	    empService.saveRegisteredUser((EmployeeModel) model); 
-	    return "redirect:/login.html?lang=" + request.getLocale().getLanguage(); 
-	}
 
 	
 
